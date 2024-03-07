d63a45100ca8fc422f70367817c1b0791bc0ae4f
Yohannes <johnherma72b@gmail.com>
2024-03-06 21:16:08 -0900

    I will continue tomorrow

--- bank_account2.py
+++ bank_account2.py
@@ -3,6 +3,18 @@ class BankAccount():
         self.name = name
         self.balance = balance
         print(f"\nAccount '{self.name}' is created with ${self.balance} initial amount")
+    #Depositing process
     def deposit(self, amount):
         self.balance = self.balance + amount
-        print(f"{self.name} is added $ {amount}  deposit and now it has $ {self.balance} amount of balance ")
+        print(f"\n{self.name} is added $ {amount}  deposit and now it has $ {self.balance} amount of balance ")
+    #Withdrawal process
+    def leftBalance(self, left):
+        if self.balance >= left:
+            return
+        else:
+            print(f"Sorry your balance is insufficient")
+    def withdraw(self, withdraw):
+        self.leftBalance(amount)
+        self.balance = self.balance - withdraw
+        print(f"\n{self.name} is withdraw ${withdraw} and now it has ${self.balance} amount of balance")
+
--- oop_project2.py
+++ oop_project2.py
@@ -5,3 +5,5 @@ from bank_account2 import *
 Melon = BankAccount("Melon", 300000)
 John = BankAccount("John", 400000)
 Melon.deposit(4000)
+
+Melon.withdraw(4008888)
