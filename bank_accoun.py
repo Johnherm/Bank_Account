badc30d1a03d0fa8775222a51017b9987ccfa92e
Yohannes <johnherma72b@gmail.com>
2024-03-06 17:40:14 -0900

    Deleted bank account due to poor name

--- bank_accoun.py
+++
@@ -1,6 +0,0 @@
-class BankAccount():
-    def __init__(self, initialAmount, acctName):
-        self.balance = initialAmount
-        self.name = acctName
-        print(f"\nAccount'{self.name}' created.")
-        print(f"Balance= ${self.balance:.2f}")
